56aca256fd30e21e236bb4b3d630387a9558131e
Porfirio Rodriguez <porfiriorodt@gmail.com>
2021-05-17 20:39:00 -0400

    creacion de 4 nuevos test sobre el programa de los anagramas y funcionales

--- AreAnagrams/UnitTest1.cs
+++ AreAnagrams/UnitTest1.cs
@@ -19,6 +19,46 @@ namespace AreAnagrams
 
         }
 
+        [Test]
+        public void AreNotAnagramas()
+        {
+
+            Program anagrams = new Program(); 
+
+            Assert.That(() => anagrams.IsAnAnagram("Juan", "pedro"), Is.False /* Throws.Nothing*/);
+
+        }
+
+        [Test]
+        public void FirstAnagramWordIsNull()
+        {
+
+            Program anagrams = new Program(); 
+
+            Assert.That(() => anagrams.IsAnAnagram(null, "pedro"), Throws.ArgumentNullException);
+
+        }
+
+        [Test]
+        public void SecondAnagramWordIsNull()
+        {
+
+            Program anagrams = new Program(); 
+
+            Assert.That(() => anagrams.IsAnAnagram("pedro", null), Throws.ArgumentNullException);
+
+        }
+
+        [Test]
+        public void BothAnagramWordsAreNull()
+        {
+
+            Program anagrams = new Program(); 
+
+            Assert.That(() => anagrams.IsAnAnagram(null, null), Throws.ArgumentNullException);
+
+        }
+
 
     }
 }
