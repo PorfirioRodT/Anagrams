22303aa1e2d0705a7a4d2ac84c70d38502e9774d
Porfirio Rodriguez <porfiriorodt@gmail.com>
2021-05-17 20:24:33 -0400

    Creacion del proyecto de Anagramas

---
+++ AreAnagrams/UnitTest1.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+
+namespace AreAnagrams
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+
+            Assert.Pass();
+
+        }
+
+
+    }
+}
